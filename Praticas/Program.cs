562014faa49ab9b260763414926b370d776fe104
Babaloricha <51730856+Babaloricha@users.noreply.github.com>
2020-09-16 10:29:10 -0300

    novos jogos (#1)
    * mais 1 jogo
    
    * mais outro jogo
    
    * correcao da numeracao
    
    Co-authored-by: Joao Verissimo Felippe De Melo <j.felippe.de.melo@avanade.com>

--- Praticas/Program.cs
+++ Praticas/Program.cs
@@ -17,7 +17,10 @@ namespace Praticas
             listaJogos.Add(new Jogo(5, "MineCraft", Classificao.bom));
             listaJogos.Add(new Jogo(6, "League of Legends", Classificao.bom));
             listaJogos.Add(new Jogo(4, "CS", Classificao.ruim));
-            listaJogos.Add(new Jogo(7, "The Sims", Classificao.bom));
+            listaJogos.Add(new Jogo(8, "The Sims", Classificao.bom));
+            listaJogos.Add(new Jogo(9, "The Sims", Classificao.bom));
+            listaJogos.Add(new Jogo(10, "The Sims", Classificao.bom));
+
             monitora("fim");
 
         }
